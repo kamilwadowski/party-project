c72480b149123b82dcecaf7a2e81c3bf70b729a4
Kamil Wadowski <wadowski@mail.de>
2022-08-31 13:51:05 +0200

    added text

--- src/field/Program.java
+++ src/field/Program.java
@@ -6,11 +6,12 @@ public class Program {
 
         Field field1 = new Field();
 
+
         field1.buildGame();
         field1.printGame();
         System.out.println("##############");
         field1.buildField();
         System.out.println("##############");
-
+        System.out.println("to jest proba kolejnego commita");
     }
 }
