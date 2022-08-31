6566f151c72ed9155768fbe1b6e422c1b0571477
Kamil Wadowski <wadowski@mail.de>
2022-08-31 13:23:13 +0200

    added program

---
+++ src/field/Program.java
@@ -0,0 +1,43 @@
+package field;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Field5 field5 = new Field5();
+        Field field1 = new Field();
+
+        field1.buildGame();
+        field1.printGame();
+        System.out.println("##############");
+        field1.buildField();
+        System.out.println("##############");
+        field5.buildField();
+        field5.printField();
+
+
+
+
+
+       /* field4.buildNewField();
+        field4.printNewField();
+        field4.buildst();
+        field4.printst();
+        field4.buildFieldChar();
+        field4.printFieldChar();
+        Field field1 = new Field();
+        Field2 field2 = new Field2();
+        Field3 field3 = new Field3();
+        Field4 field4 = new Field4();
+        field4.buildField();
+        field4.printField();
+        field1.buildField();
+        field1.buildGame();
+        field1.printGame();
+
+        field2.buildFeld();
+        field2.printFeld();
+        System.out.println();
+        field3.tabelaStudenta();
+*/
+    }
+}
